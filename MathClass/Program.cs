59a2008b55303e0e9b132a669d114ec3eafd10c4
kysonhensley <hensleykyson@gmail.com>
2025-09-15 08:43:10 -0600

    Add project files.

---
+++ MathClass/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MathClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 5;
+
+            Console.WriteLine(Math.Sqrt(firstNumber) * Math.Sqrt(firstNumber));
+
+            Console.WriteLine(Math.Pow(2, 2));
+            
+            
+            
+            //pause
+            Console.Read();
+        }
+    }
+}
